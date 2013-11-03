52244ce411e09cda5bb9892ce24fbb71f6a89c8a
Seonggwan Lee <seonggwan.lee@gmail.com>
2013-11-04 02:49:16 +0900

    Remove some comments

--- get_products.rb
+++ get_products.rb
@@ -7,13 +7,7 @@ url = Hash.new('http://www.possess.kr')
 # Example img url = http://www.possess.kr/shopimages/flfl0114/0020000002403.jpg
 url['Dress'] = 'http://www.possess.kr/shop/shopbrand.html?xcode=002&type=O'
 doc = Nokogiri::HTML(open(url['Dress']))
-# doc.css(".prdList .MS_prod_img_s").each do |img|
-# 	img_url = url['Possess'] + img['src']
-# 	img_name = img['src'].gsub(/\//, '_')
-# 	open(img_name, 'wb') do |file|
-# 		file << open(img_url).read
-# 	end
-# end
+
 
 products = []
 puts doc.at_css(".pbox")
@@ -38,16 +32,6 @@ open("product.txt", 'w') do |file|
 end
 
 
-
-
-# default_url = "http://www.possess.kr"
-# url = "http://www.possess.kr/shop/shopdetail.html?branduid=670172&xcode=005&mcode=000&scode="
-# doc = Nokogiri::HTML(open(url))
-# img_src = doc.at_css("#detailTop img")['src']
-# img_url = default_url + img_src
-# img_name = img_src.gsub(/\//, '_')
-# puts img_url
-# puts img_name
 
 
 
