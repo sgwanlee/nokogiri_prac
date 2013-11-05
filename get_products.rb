2ff3eab90db1c39c2a85ce2dd57ccb0391c9e38b
Seonggwan Lee <seonggwan.lee@gmail.com>
2013-11-06 01:14:49 +0900

    Grab all pages with the gem Mechanize

--- get_products.rb
+++ get_products.rb
@@ -1,34 +1,44 @@
 require 'rubygems'
+require 'mechanize'
 require 'nokogiri'
 require 'open-uri'
 
 url = Hash.new('http://www.possess.kr')
 # Dress product image (first page)
 # Example img url = http://www.possess.kr/shopimages/flfl0114/0020000002403.jpg
-url['Dress'] = 'http://www.possess.kr/shop/shopbrand.html?xcode=002&type=O'
-doc = Nokogiri::HTML(open(url['Dress']))
+url['Dress'] = '/shop/shopbrand.html?xcode=002&type=O'
+# doc = Nokogiri::HTML(open(url['Dress']))
 
+agent = Mechanize.new
+page = agent.get(url[:default])
+page = agent.page.link_with(href: url['Dress']).click
+
+pagination = agent.page.links_with(:href => /page=/)[0..-2]
 
 products = []
-puts doc.at_css(".pbox")
 
-doc.css(".pbox").each do |product|
+pagination.each do |page|
+	page = page.click
 
-	img_src = product.at_css('.prdimg img')['src']
-	img_url = url['img'] + img_src
-	price = product.css('.prdPrice').text
-	price = price.gsub(/.*→/, '')
-	img_name = img_src.gsub(/\//, '_')
-	products << {img: img_url, price: price}
-	open(img_name, 'wb') do |file|
-		file << open(img_url).read
+	page.search(".pbox").each do |product|
+		# Get image & price info.
+		img_src = product.at_css('.prdimg img')['src']
+		img_url = url['img'] + img_src
+		price = product.css('.prdPrice').text.gsub(/.*→/, '')
+
+		products << {img: img_url, price: price}
+		
+		Save image
+		open(img_src.gsub(/\//, '_'), 'wb') do |file|
+			file << open(img_url).read
+		end
 	end
-end
 
+end 
+
+# Save price info.
 open("product.txt", 'w') do |file|
-	products.each do |product|
-		file << product
-	end
+	file << products
 end
 
 
